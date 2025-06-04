4facdfd9e7e8c6d8d495905aeb348c687b531e8f
hael0725 <hael0725@naver.com>
2025-06-04 18:12:53 +0900

    20250604

---
+++ js/sub_book1.js
@@ -0,0 +1,16 @@
+$.ajax({
+    method: "GET",
+    url: `https://dapi.kakao.com/v3/search/book?target=title`,
+    data: { query: "구병모", size: 6 },
+    headers: { Authorization: "KakaoAK 3e72338bf5f39f0b5513f22abd6a2ed7" }
+})
+    .done(function (msg) {
+
+        for (var i = 0; i < 23; i++) {
+            $(".wrap3 .imgbox").eq(i).append("<img src ='" + msg.documents[i].thumbnail + "'/>");
+            $(".wrap3 .title").eq(i).append("<h5>" + msg.documents[i].title.substring(0, 14) + "</h5>");
+            $(".wrap3 .by").eq(i).append(msg.documents[i].authors[0]);
+            var title = msg.documents[i].title;
+
+        }
+    });
